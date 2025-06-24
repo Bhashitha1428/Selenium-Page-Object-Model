ec8fb6e8daef190bcf6278ee181d897271028f1b
Bhashitha1428 <bhashithamiyurantha.bmr@gmail.com>
2025-06-24 10:47:47 +0530

    Initial commit

---
+++ src/main/java/pages/RegisterPage.java
@@ -0,0 +1,56 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class RegisterPage {
+
+    WebDriver driver;
+
+    public RegisterPage(WebDriver driver){
+        this.driver=driver;
+    }
+
+      By firstName_loc=By.xpath("//input[@name='firstName']");
+      By latsName_loc=By.xpath("//input[@name='lastName']");
+      By phone_loc=By.xpath("//input[@name='phone']");
+      By userName_loc= By.xpath("//input[@id='userName']");
+      By country_loc=  By.xpath("//select[@name='country']");
+      By email_loc=  By.xpath("//input[@id='email']");
+      By pwd_loc=By.xpath("//input[@name='password']");
+      By confirmPwd_loc=By.xpath("//input[@name='confirmPassword']");
+      By submit_loc=  By.xpath("//input[@name='submit']");
+
+    public void setFirstName(String firstName){
+     driver.findElement(firstName_loc).sendKeys(firstName);
+    }
+    public void setLastName(String lastName){
+        driver.findElement(latsName_loc).sendKeys(lastName);
+    }
+    public void setPhoneNumber(String phoneNumber){
+        driver.findElement(phone_loc).sendKeys(phoneNumber);
+    }
+    public void setUserName(String userName){
+        driver.findElement(userName_loc).sendKeys(userName);
+    }
+    public void setCountry(){
+        WebElement countryDropdown=driver.findElement(country_loc);
+        Select countryList=new Select(countryDropdown);
+        countryList.selectByIndex(5);
+    }
+    public void setEmail(String email){
+        driver.findElement(email_loc).sendKeys(email);
+    }
+    public void setPassword(String password){
+        driver.findElement(pwd_loc).sendKeys(password);
+    }
+    public void setConfirmPassword(String confirmPassword){
+        driver.findElement(confirmPwd_loc).sendKeys(confirmPassword);
+    }
+
+    public void submit(){
+        driver.findElement(submit_loc).click();
+    }
+}
---
+++ src/test/java/testcases/TC001_RegisterUserTest.java
@@ -0,0 +1,53 @@
+package testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.RegisterPage;
+import pages.RegisterSuccessPage;
+
+import java.time.Duration;
+
+public class TC001_RegisterUserTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void openPage(){
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://demo.guru99.com/test/newtours/index.php");
+    }
+
+   @Test
+    public void TC001() throws InterruptedException {
+
+        HomePage homePage=new HomePage(driver);
+        homePage.selectRegisterMenu();
+
+       RegisterPage registerPage=new RegisterPage(driver);
+       registerPage.setFirstName("Bhashitha");
+       registerPage.setLastName("Ranchagoda");
+       registerPage.setPhoneNumber("0716779857");
+       registerPage.setEmail("bmr@gmail.com");
+       registerPage.setCountry();
+       registerPage.setUserName("bmr");
+       registerPage.setPassword("123456");
+       registerPage.setConfirmPassword("123456");
+
+       Thread.sleep(5000);
+       registerPage.submit();
+
+       RegisterSuccessPage registerSuccessPage=new RegisterSuccessPage(driver);
+       String actualText=registerSuccessPage.getRegisterSuccessText();
+       Assert.assertTrue(actualText.contains("Dear"),"Registration Attempt failed");
+
+    }
+
+}
